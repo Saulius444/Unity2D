99b69b32adbbd643b89e70e321f43954fe71ec5a
Saulius444 <Saulius444@users.noreply.github.com>
2018-10-26 10:58:51 +0300

    First Commit
    File on "C:\Users\Saulius\Documents\Repositories\Unity2D\Text101\Text101\Library\il2cpp_cache\linkresult..." was rared

---
+++ Number_Wizzard_Console/NUmber Wizzard Console/Assets/NumberWizzard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizzard : MonoBehaviour {
+
+	int maxNum;
+	int minNum;
+	int guess;
+
+	// Use this for initialization
+	void Start () {
+
+		StartGame();
+	}
+
+	void StartGame() {
+
+		maxNum = 1000;
+		minNum = 1;
+		guess = 500;
+
+		Debug.Log("Welcome to number wizzard game.");
+		Debug.Log("Think of a number...");
+		Debug.Log("Highest possible number is: " + maxNum);
+		Debug.Log("Lowest possible number is: " + minNum);
+		Debug.Log("Tell me if your number is higher or lower than: " + guess);
+		Debug.Log("Push UP = Higher, Push DOWN = Lower, Push ENTER = Correct");
+		maxNum = maxNum + 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+
+			minNum = guess;
+			NextGuess();
+		}
+
+		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+
+			maxNum = guess;
+			NextGuess();
+		}
+
+		else if (Input.GetKeyDown(KeyCode.Return)) {
+
+			Debug.Log("I am a genius.");
+			StartGame();
+		}
+	}
+
+	void NextGuess () {
+
+		guess = (maxNum + minNum) / 2;
+		Debug.Log("Is it higher or lower than... " + guess);
+	}
+}
---
+++ Number_Wizzard_UI/NUmber Wizzard UI/Assets/Scripts/NumberWizzard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizzard : MonoBehaviour {
+
+	[SerializeField] int maxNum;
+	[SerializeField] int minNum;
+	[SerializeField] TextMeshProUGUI guessText;
+	int guess;
+
+	// Use this for initialization
+	void Start () {
+
+		StartGame();
+	}
+
+	void StartGame() {
+
+		NextGuess();
+	}
+	
+	public void OnPressHigher() {
+
+		minNum = guess +1;
+		NextGuess();
+	}
+
+	public void OnPressLower() {
+
+		maxNum = guess -1;
+		NextGuess();
+	}
+
+	void NextGuess () {
+
+		guess = Random.Range(minNum, maxNum +1);
+		guessText.text = guess.ToString();
+	}
+}
